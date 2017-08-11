2dad07e965139133a14f99b2b1b5fba7e2ae7370
behzad khosravifar <behzadkh@hotmail.com>
2017-08-11 18:11:45 +0430

    First init

---
+++ src/Design.pro/Design.pro/Helpers/BotHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using TeleBanel;
+
+namespace Design.pro.Helpers
+{
+    public static class BotHelper
+    {
+
+        public static IWebHostBuilder UseTelegramBot(this IWebHostBuilder webHostBuilder, string botApiKey, string botPass)
+        {
+            var bot = new BotManager(botApiKey, botPass);
+            bot.StartListening();
+
+            return webHostBuilder;
+        }
+    }
+}
---
+++ src/Design.pro/Design.pro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Design.pro.Helpers;
+using Design.pro.Properties;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Design.pro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseTelegramBot(Resources.BotApiKey, Resources.BotApiPassword)
+                .Build();
+
+            host.Run();
+
+        }
+    }
+}
